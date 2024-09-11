1618b595ca888d33db2d4649b6ae2ce36bf2985b
jakob <jakob.rottstegge@gmail.com>
2024-09-11 22:37:53 +0200

    Simple Navbar Component; Temporäre NavbarItem Namen; Tempörar im Login und Navigation View hinzugefügt

---
+++ new-project/src/components/NavItem.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <a :href="getNavItemUrl()" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">{{ props.itemLabel }}</a>
+</template>
+
+<script setup lang="ts">
+
+  const props = defineProps({
+    itemLabel: String,
+    urlRef: String
+  });
+
+  function getNavItemUrl() {
+    return window.location.origin + props.urlRef;
+  }
+</script>
+
+<style scoped>
+
+</style>
