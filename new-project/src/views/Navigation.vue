d7aab40f8c0e6dcbe1786a477670fe4e8de3fff0
Tuxfan <jonas@familie-wuepping.de>
2024-09-11 22:13:46 +0200

    Add default page to redirect to login

--- new-project/src/views/Navigation.vue
+++ new-project/src/views/Navigation.vue
@@ -3,7 +3,12 @@
 </template>
 
 <script setup lang="ts">
+import {onMounted} from "vue";
+import router from "../router";
 
+onMounted(() => {
+  router.push("/login")
+})
 </script>
 
 <style scoped>
