eac6d083aece95edc4aee45e065fbf56afe5da1d
Tuxfan <jonas@familie-wuepping.de>
2024-09-11 22:12:23 +0200

    Add login page

--- new-project/src/views/Login.vue
+++ new-project/src/views/Login.vue
@@ -1,24 +1,9 @@
 <template>
-  <div>
-    <fieldset class="bg-gray-200 grid grid-cols-1">
-      <PlayArea class="absolute size-fit m-auto"/>
-      <div :class="'left-['+bla+ 'rem] -top-['+bla2+'px]'" class="size-10 bg-red-500 relative"
-      />
-    </fieldset>
+  <div class="h-dvh bg-gradient-to-r from-cyan-500 to-blue-600 flex justify-center">
+    <LoginField class="w-1/6 h-1/2 my-auto"/>
   </div>
 </template>
 <script setup lang="ts">
-import PlayArea from "../components/PlayArea.vue";
-import {ref} from "vue";
-
-const bla = 10;
-const bla2 = 20;
-const playerPositionX = ref<number>(80);
-const playerPositionY = ref<number>(48);
-
-function getCssDataForPlayerPosition() {
-  return "left-[" + playerPositionX.value.toString() + "rem] top-[" + playerPositionY.value.toString() + "rem]";
-}
-
+import LoginField from "../components/LoginField.vue";
 </script>
 <style scoped></style>
