127b14dda8ec3362e823943605fc6b44b9a9add1
Tuxfan <jonas@familie-wuepping.de>
2024-09-13 15:09:32 +0200

    Add component to view

--- new-project/src/views/Home.vue
+++ new-project/src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
   <Navbar/>
+  <div class="w-full">
+    <YearChart class="m-auto"/>
+  </div>
 </template>
 
 <script setup lang="ts">
 
 import Navbar from "../components/Navbar.vue";
+import YearChart from "../components/YearChart.vue";
 </script>
 
 <style scoped>
